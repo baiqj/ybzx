0481c4edff99e9cea6a591d9d9bf44ee3154f23e
xiangkeguo@gmail.com <xiangkeguo@gmail.com>
2014-06-23 21:45:47 +0800

    images path&olark

--- sites/all/themes/b3/templates/groups/node--2.tpl.php
+++ sites/all/themes/b3/templates/groups/node--2.tpl.php
@@ -9,21 +9,21 @@
 				  <!-- Carousel items -->
 				  <div class="carousel-inner">
 				    <div class="active item">
-				      	<img src="/<?php echo drupal_get_path('theme', 'b3');?>/images/family_2.png">
+				      	<img src="<?php echo base_path().drupal_get_path('theme', 'b3');?>/images/family_2.png">
 				      	<div class="carousel-caption">
 							    <h3>因此，人要离开父母与妻子联合，二人成为一体。--创世纪 2:24</h3>
 							    <!-- <p> </p> -->
 							  </div>		    	
 				    </div>
 				    <div class="item">
-				      	<img src="/<?php echo drupal_get_path('theme', 'b3');?>/images/family_3.png">
+				      	<img src="<?php echo base_path().drupal_get_path('theme', 'b3');?>/images/family_3.png">
 				      	<div class="carousel-caption">
 							    <h3>耶和华神说，那人独居不好，我要为他造一个配偶帮助他。--创世纪 2:18</h3>
 							    
 							  </div>		    	
 				    </div>
 				    <div class="item">
-				      	<img src="/<?php echo drupal_get_path('theme', 'b3');?>/images/family_1.png">
+				      	<img src="<?php echo base_path().drupal_get_path('theme', 'b3');?>/images/family_1.png">
 				      	<div class="carousel-caption">
 							    <h3>然而你们各人都当爱妻子，如同爱自己一样。妻子也当敬重她的丈夫。</h3>
 							    <p>--以弗所书5:33</p>
